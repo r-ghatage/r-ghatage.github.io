a694a89abe15be06b2484d1a5cb4fa5ac6adbcd9
RG <raghavendra.ghatage@gmail.com>
2025-07-18 23:19:49 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Service Worker for handling notifications
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({type: 'window'}).then(windowClients => {
+      for (const client of windowClients) {
+        if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
